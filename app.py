2d87b7c54e5e82267669708ae9149893e806f3aa
Thien Ngan Doan <ngandn18@gmail.com>
2025-05-07 09:13:58 +0700

    Update app.py
    Add variable PORT

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import os
+
+PORT = int(os.environ.get("PORT", 5000))  # Use Render's assigned PORT
 
 app = Flask(__name__)
 
@@ -7,5 +10,5 @@ def home():
     return "Docker is working!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=PORT, debug=True)
 
